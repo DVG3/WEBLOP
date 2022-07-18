45ec2b2cf68106eb4b1724afe96a5087ef50ff22
DVG3 <lapminh02022006@gmail.com>
2022-07-19 01:10:30 +0700

    bruh

--- Server.py
+++ Server.py
@@ -1,1 +1,12 @@
 from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route('/easteregg')
+def easteregg():
+    return {"message":"Hello world"}
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- weblopv1/src/App.js
+++ weblopv1/src/App.js
@@ -1,24 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React,{useState, useEffect} from 'react'
 function App() {
+  const [data,setData] = useState([{}])
+
+  useEffect(()=>{
+    fetch('/easteregg').then(
+      res => res.json()
+    ).then(
+      data => {
+        setData(data);
+        console.log(data);
+      }
+    )
+  }
+  )
+  console.log(data.easteregg)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {(typeof data.easteregg === undefined)?(
+        <p>loading...</p>
+      )  :(
+        <p>data.easteregg</p>
+      )
+    }
     </div>
+
   );
 }
 
